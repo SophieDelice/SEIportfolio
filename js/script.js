4056c949d0cea7759345b63d9f61c3010f674d46
Sophie Delice <delicesophie824@gmail.com>
2023-05-13 18:02:56 -0400

    wip:adds filesystem

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 // alert('js is loaded')
+
+// Get the <body> element
+// var body = document.body;
+
+// // Function to toggle day and night modes
+// function toggleMode() {
+//   body.classList.toggle("night");
+// }
