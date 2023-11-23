8c5e18063203f537f832e4d21fb63c9ef58a25b2
Sophie Delice <delicesophie824@gmail.com>
2023-11-22 19:59:11 -0500

    wip:adds filesystem

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 //  alert('js is loaded')
 
 
-const body = document.querySelector("body");
-const cb1 = document.getElementById("cb1");
-console.log(body)
+// const body = document.querySelector("body");
+// const cb1 = document.getElementById("cb1");
+// console.log(body)
 
 
-cb1.addEventListener("click", () => {
- body.classList.toggle("day-background");
- console.log("click")
- });
+// cb1.addEventListener("click", () => {
+//  body.classList.toggle("day-background");
+//  console.log("click")
+//  });
 
